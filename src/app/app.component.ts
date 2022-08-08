4f7d9644be7812f25b865f4270c910548bb107eb
Israel Sanchez <Israel.pro1994@gmail.com>
2022-08-08 11:04:48 -0500

    create post function moved to posts.service file

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,9 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
-import { Post } from './post.model';
-
 import { map } from 'rxjs/operators';
+
+import { PostsService } from './posts.service';
+import { Post } from './post.model';
 
 @Component({
   selector: 'app-root',
@@ -14,7 +15,7 @@ export class AppComponent implements OnInit {
   loadedPosts: Post[] = [];
   isFetching = false;
 
-  constructor( private http: HttpClient ) {}
+  constructor( private http: HttpClient, private postsService: PostsService ) {}
 
   ngOnInit() {
     // whenever this page/app loads, I want to fetch all posts
@@ -22,26 +23,26 @@ export class AppComponent implements OnInit {
   }
 
   // { title: string; content: string } - moved to post.model.ts file!
+  // NOTE: we have moved the function below to posts.service.ts 
   onCreatePost( postData: Post ) {
-    // Send Http request
+    this.postsService.createAndStorePost( postData.title, postData.content );
+{/*
     console.log(postData);
     this.http
-      .post<{ name: string }>( // again all http methods are genertic which means they can have optional brackets for a specific type
-        // 'https://console.firebase.google.com/project/angular-firebase-ff3a9/database/angular-firebase-ff3a9-default-rtdb/data,
-        'https://angular-firebase-ff3a9-default-rtdb.firebaseio.com/posts.json', // .json is a requirement when using Firebase..
+      .post<{ name: string }>( 
+        'https://angular-firebase-ff3a9-default-rtdb.firebaseio.com/posts.json',
         postData
       )
+*/}
       // angular is HEAVILY reliant on Observables - same with HTTP 
       // we subscribe to get informed about the response and to handle errors and so on
       // if we DON'T subscribe - Angular and RxJS will know that no one's interested in the response therefore
-      //  the request doesn't even get sent because if no one's intrested in the response then it won't send the request!
+      //  the request doesn't even get sent because if no one's intrested in the response then it won't send the request!      
+{/*
       .subscribe( responseData => { 
-        // the httpClient will give you more than just the response
-        // it will automatically extract the data attached to the response (the repsonse body)
-        console.log(responseData);
-        // if you visit the url below - you can see the newly created posts with their title and content 
-        // https://console.firebase.google.com/project/angular-firebase-ff3a9/database/angular-firebase-ff3a9-default-rtdb/data        
+        console.log(responseData); 
       }); 
+*/}
       // in the web browser - developer tools - network
       // we can see two requests to the POST end point
       // note: its not '/post' but TYPE of post - the .post() above
--- src/app/posts.service.ts
+++ src/app/posts.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
+
 import { Post } from "./post.model";
 
 @Injectable({
@@ -9,16 +10,21 @@ export class PostsService {
 
     constructor( private http: HttpClient ) {}
 
-    createAndStorePost(title: string, content: string){
-    const postData: Post = {};
-    this.http
-        .post<{ name: string }>( 
-            'https://angular-firebase-ff3a9-default-rtdb.firebaseio.com/posts.json', 
-            postData
-        )     
-      .subscribe( responseData => {  
-        console.log(responseData);  
-      }); 
+    // Original funciton with notes in app.component.ts file
+    createAndStorePost( title: string, content: string ) {
+        const postData: Post = { title: title, content: content };
+        this.http
+            .post<{ name: string }>( // again all http methods are genertic which means they can have optional brackets for a specific type
+                'https://angular-firebase-ff3a9-default-rtdb.firebaseio.com/posts.json', // .json is a requirement when using Firebase..
+                postData
+            )     
+        .subscribe( responseData => {  
+            console.log(responseData);  
+            // the httpClient will give you more than just the response
+            // it will automatically extract the data attached to the response ( the repsonse body )
+            // if you visit the url below - you can see the newly created posts with their title and content 
+            // https://console.firebase.google.com/project/angular-firebase-ff3a9/database/angular-firebase-ff3a9-default-rtdb/data       
+        }); 
     };
 
     fetchPosts(){
