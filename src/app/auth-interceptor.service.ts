a8a2547daeffed56741b19986d09b7a5ddf18984
Israel Sanchez <Israel.pro1994@gmail.com>
2022-08-10 15:47:18 -0500

    response interceptors - similar to what we have inside of post.service delete function

--- src/app/auth-interceptor.service.ts
+++ src/app/auth-interceptor.service.ts
@@ -1,5 +1,6 @@
-import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+import { HttpEvent, HttpEventType, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
 import { Observable } from "rxjs";
+import { tap } from "rxjs/operators";
 
 export class AuthInterceptorService implements HttpInterceptor{
 
@@ -7,7 +8,7 @@ export class AuthInterceptorService implements HttpInterceptor{
     // HttpRequest is a generic type/generic object so we can use the angled brackets 
     intercept( req: HttpRequest<any>, next: HttpHandler ) {
         console.log('Request is on the way');
-        console.log(req.url);
+        console.log(req.url, 'this is the req.url');
         // req.url = ''; <== this will not work - we need to create a new one!
         const modifedRequest = req.clone({ // with clone we can now change core aspects such as:
             // url: 'some-new-url'
@@ -17,9 +18,21 @@ export class AuthInterceptorService implements HttpInterceptor{
             // now when we check in the browser web dev tools -- Network -> (second) post -> Headers
             // we can now see Auth: xyz inside the Request Headers
          }); 
+        
+        console.log(modifedRequest, 'this is the modifedRequest');         
+         // we're not limited to interacting with the REQUEST in an interceptor
+         // we can also interact with the RESPONSE
          // instead of using the origianl request we want to use the MODIFIED VERSION - duh why make it and not use it?
-        console.log(modifedRequest);
-        return next.handle(modifedRequest); // next is a function/object that allows the request to continue its journey
+        return next.handle(modifedRequest)  // next is a function/object that allows the request to continue its journey
+                .pipe( 
+                    tap( event => {
+                        console.log(event, 'this is the event');
+                        if( event.type === HttpEventType.Response ){
+                            console.log('Response arrived, body data: ');
+                            console.log(event.body);
+                        }
+                    })
+                );        
     };
 
 };
