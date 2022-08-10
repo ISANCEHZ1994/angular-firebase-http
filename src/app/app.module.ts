08cbeff1260a6e8fe529aac27d9f17c6015a23b5
Israel Sanchez <Israel.pro1994@gmail.com>
2022-08-10 15:02:10 -0500

    introducing interceptors and manipulating request objects

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,26 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
+import { AuthInterceptorService } from './auth-interceptor.service';
 
 @NgModule({
   declarations: [AppComponent],
   imports: [BrowserModule, FormsModule, HttpClientModule],
-  providers: [],
+  // we created the file auth-interceptor and now need to add a new element - javascript object
+  // we need to add three keys:
+  // provide: HTTP_INTERCEPTORS - this will be the token by which this injection can later be identified
+  // now all the classes that i will provide on the token should be treated as HTTP Interceptors and should run
+  // their intercept method whenever a request leaves the application
+  // useClass: <SERVICE NAME WE CREATED>
+  // multi: true - have multiple services under that identifier <HTTP_INTERCEPTORS>
+  providers: [{
+    provide:  HTTP_INTERCEPTORS,
+    useClass: AuthInterceptorService,
+    multi:    true
+  }],
   bootstrap: [AppComponent]
 })
 export class AppModule {}
---
+++ src/app/auth-interceptor.service.ts
@@ -0,0 +1,25 @@
+import { HttpEvent, HttpHandler, HttpInterceptor, HttpRequest } from "@angular/common/http";
+import { Observable } from "rxjs";
+
+export class AuthInterceptorService implements HttpInterceptor{
+
+    // intercept takes two arguments
+    // HttpRequest is a generic type/generic object so we can use the angled brackets 
+    intercept( req: HttpRequest<any>, next: HttpHandler ) {
+        console.log('Request is on the way');
+
+        // req.url = ''; <== this will not work - we need to create a new one!
+        const modifedRequest = req.clone({ // with clone we can now change core aspects such as:
+            // url: 'some-new-url'
+            // headers: <if you want to keep some of them the same> req.headers.append
+            // params: -- as you can see we can change a lot of things here!
+            headers: req.headers.append('Auth', 'xyz')
+            // now when we check in the browser web dev tools -- Network -> (second) post -> Headers
+            // we can now see Auth: xyz inside the Request Headers
+         }); 
+         // instead of using the origianl request we want to use the MODIFIED VERSION - duh why make it and not use it?
+
+        return next.handle(modifedRequest); // next is a function/object that allows the request to continue its journey
+    };
+
+};
